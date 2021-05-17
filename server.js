5bff436e78845cb22f7774c6aab3d4a7dfebb1a3
Pardeep Bhatt <pardeep.b.interns@aux.gojek.com>
2021-05-17 15:16:09 +0530

    added docker

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const expressValidator = require("express-validator");
 const cookieParser = require("cookie-parser");
@@ -12,7 +11,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect("mongodb://mongo:27017/TodoApp", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -21,13 +20,16 @@ mongoose
 
 // Middleware
 app.use(morgan("dev")); // logger for terminal
-app.use(bodyParser.json()); // to read the data
+app.use(express.json()); // to read the data
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(expressValidator());
 
 app.use("/", require("./Routes/user"));
 
-app.listen(`${process.env.PORT}`, function() {
-  console.log(`Running on port ${process.env.PORT}`);
+const PORT = 3000
+
+app.listen(PORT, function() {
+  console.log(`Running on port ${PORT}`);
 });
